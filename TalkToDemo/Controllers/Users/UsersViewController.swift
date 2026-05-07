1536087e3ccaffbf757716580d6df0fcf32bcaa5
Nurul <nurul.islam@ellingsoncompanies.com>
2026-05-07 12:10:09 +0600

    changes model ref

--- TalkToDemo/Controllers/Users/UsersViewController.swift
+++ TalkToDemo/Controllers/Users/UsersViewController.swift
@@ -146,8 +146,7 @@ extension UsersViewController: UITableViewDelegate, UITableViewDataSource {
         guard indexPath.row < viewModel.filteredUsers.count, !spinner.isAnimating else { return }
         let user = self.viewModel.filteredUsers[indexPath.row]
         viewModel.updateReadStatus(user)
-        //RoutingService.shared.navigateToProfileView(user)
-        RoutingService.shared.showShimmerEffect()
+        RoutingService.shared.navigateToProfileView(user)
     }
 }
 
